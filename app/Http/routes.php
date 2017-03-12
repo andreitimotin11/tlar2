304ce5e23a4cd8006717f408dcbc557d1b0be967
Andrei Timotin <andreitimotin11@gmail.com>
2017-03-12 18:07:43 +0200

    Controllers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
     print 'sad';
-});
+})->name('home');
 Route::get('/about', 'FirstController@show');
 Route::get('/article/{page}', ['uses'=>'FirstController@show', 'as'=> 'article', 'middleware'=> 'mymiddle']);
